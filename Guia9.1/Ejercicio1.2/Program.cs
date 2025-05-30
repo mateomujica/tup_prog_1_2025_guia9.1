bf631d07f5d4320ffdf56caa47e60e0d032d687b
mateomujica <mateomujicam@gmail.com>
2025-05-30 15:27:14 +0200

    subida

--- Guia9.1/Ejercicio1.2/Program.cs
+++ Guia9.1/Ejercicio1.2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //vect
+            nombres = new string[100]
+            libreta = new int[100]
+            notas = new double[100]
+
+            // A
+            double promedio = 0;
+
+            //B
+            double mayor = 0;
+            double menor = 999.9;
+            string primermen, primermay; // nombre primer menor y mayor
+            int libretamen=999.9, libretamay=0; // num libreta menor y mayor 
+            double notamen=999.9, notamay=0; //nota menor y mayor 
+            
+
+            int libret = 0;
+            int nota = 0;
+
+            //C
+
+            while ()
+
+            
+           
+
+
+                
+            
         }
     }
 }
